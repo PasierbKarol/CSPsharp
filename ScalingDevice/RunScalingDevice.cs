20ca49f2dd85160f36b5abdde82db498053a2413
karolpasierb <lefanu@vivaldi.net>
2018-10-19 16:18:46 +0100

    Added a new project for testing scaling device. Requires parts of PlugNplay library

---
+++ ScalingDevice/RunScalingDevice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CSPlang;
+
+namespace ScalingDevice
+{
+    public class RunScalingDevice
+    {
+        static void Main(string[] args)
+        {
+            One2OneChannel data = Channel.one2one();
+            One2OneChannel timedData = Channel.one2one();
+            One2OneChannel scaledData = Channel.one2one();
+            One2OneChannel oldScale = Channel.one2one();
+            One2OneChannel newScale = Channel.one2one();
+            One2OneChannel pause = Channel.one2one();
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ScalingDevice/Scale.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSPlang;
+
+namespace ScalingDevice
+{
+    class Scale : IamCSProcess
+    {
+        int scaling = 2;
+        int multiplier = 2;
+
+        ChannelOutput outChannel;
+        ChannelOutput factor;
+        ChannelInput inChannel;
+        ChannelInput suspend;
+        ChannelInput injector;
+
+
+
+        public void run()
+        {
+            int SECOND = 1000;
+            int DOUBLE_INTERVAL = 5 * SECOND;
+            const int NORMAL_SUSPEND = 0;
+            const int NORMAL_TIMER = 1;
+            const int NORMAL_IN = 2;
+            const int SUSPENDED_INJECT = 0;
+            const int SUSPENDED_IN = 1;
+            var timer = new CSTimer();
+            var normalAlt = new ALT( [suspend, timer, inChannel]);
+            var suspendedAlt = new ALT( [injector, inChannel]);
+            var timeout = timer.read() + DOUBLE_INTERVAL;
+            timer.setAlarm(timeout);
+            int inValue;
+            ScaledData result;
+
+            while (true)
+            {
+                switch (normalAlt.priSelect())
+                {
+
+                    case NORMAL_SUSPEND:
+                        suspend.read();         // its a signal, no data content;
+                        factor.write(scaling);   //reply with current value of scaling;
+                        bool suspended = true;
+                        Console.WriteLine("Suspended");
+                        while (suspended)
+                        {
+
+                            switch (suspendedAlt.priSelect())
+                            {
+
+                                case SUSPENDED_INJECT:
+                                    scaling = (int)injector.read();   //this is the resume signal as well;
+                                    Console.WriteLine("Injected scaling is $scaling");
+                                    suspended = false;
+                                    timeout = timer.read() + DOUBLE_INTERVAL;
+                                    timer.setAlarm(timeout);
+                                    break;
+                
+
+                                case SUSPENDED_IN:
+                                    inValue = (int)inChannel.read();
+                                    result = new ScaledData();
+                                    result.original = inValue;
+                                    result.scaled = inValue;
+                                    outChannel.write(result);
+                                    break;
+                            }  // end-switch
+                        } //end-while
+                        break;
+
+
+                    case NORMAL_TIMER:
+                        timeout = timer.read() + DOUBLE_INTERVAL;
+                        timer.setAlarm(timeout);
+                        scaling = scaling * multiplier;
+                        Console.WriteLine("Normal Timer: new scaling is $scaling");
+                        break;
+
+
+                    case NORMAL_IN:
+                        inValue = (int)inChannel.read();
+                        result = new ScaledData();
+                        result.original = inValue;
+                        result.scaled = inValue * scaling;
+                        outChannel.write(result);
+                        break;
+
+
+                } //end-switch
+            }
+        }
+    }
+}
