26f285e68d8873f0fbae156cfb9c0b8695f7ce6f
karolpasierb <lefanu@vivaldi.net>
2019-02-18 10:35:24 +0000

    Crude implementation of Groovys ALT

---
+++ CSPlang/ALT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSPlang
+{
+    public class ALT
+    {
+        public ALT(Object[] objectGuards)
+        {
+            List<Guard> guards = new List<Guard>();
+            foreach (var objectGuard in objectGuards)
+            {
+                guards.Add(objectGuard as Guard);
+            }
+            Alternative alt = new Alternative(guards.ToArray());
+        }
+    }
+}
